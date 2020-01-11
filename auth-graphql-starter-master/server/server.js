878599ea34540adc3351fc88f10050ae617f1892
Sekwon Choi <schoi@netflix.com>
2020-01-10 23:25:47 -0800

    remove mongo uri

--- auth-graphql-starter-master/server/server.js
+++ auth-graphql-starter-master/server/server.js
@@ -12,7 +12,8 @@ const schema = require("./schema/schema");
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = "";
+const MONGO_URI =
+  "mongodb+srv://choipoby:shibaseki@cluster0-8ytru.mongodb.net/test?retryWrites=true&w=majority";
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
